b9aac10e2d38a49f5dc8ba3cd99e5bf791a566c1
Kevin Saji Jude <Saji_Jude-K@ulster.ac.uk>
2024-10-08 10:09:29 +0100

    Created new classes.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿class Book
+{
+
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfPages;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine("Book information");
+        Console.WriteLine("----------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Auhtor: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Book No of pages: {NoOfPages}");
+        Console.WriteLine();
+
+    }
+
+
+    static void Main(string[] args)
+    {
+
+         
+
+         Book book = new Book();
+         book.Title = "C# for beginners";
+         book.Author = "BillGates";
+         book.ISBN = "12345678";
+         book.NoOfPages = 200;
+
+
+         Book book1 = new Book();
+         book1.Title = "C# Methods and Classes";
+         book1.Author = "Microsoft";
+         book1.ISBN = "55667778";
+         book1.NoOfPages = 200;
+
+         // Output book information to the console
+
+         book.DisplayInfo();
+         book1.DisplayInfo();
+
+
+
+    }
+
+}
