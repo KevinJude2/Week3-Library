27de61377ef7a1b18b7a712c2da16569ecc1bda5
Kevin Saji Jude <Saji_Jude-K@ulster.ac.uk>
2024-10-08 10:56:53 +0100

    Updated code with the access modifiers.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,49 +1,13 @@
-﻿class Book
+﻿using Library;
+
+class Program
 {
-
-    string Title;
-    string Author;
-    string ISBN;
-    int NoOfPages;
-
-    
-
-    // Book object 
-    public Book(string bookTitle, string bookAuthor, string bookISBN, int bookNoOfPages)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-        NoOfPages = bookNoOfPages;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine("Book information");
-        Console.WriteLine("----------------");
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Auhtor: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine($"Book No of pages: {NoOfPages}");
-        Console.WriteLine();
-
-    }
-
-
     static void Main(string[] args)
     {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
 
-         Book book = new Book("C# for beginners", "Bill Gates", "1234567", 200);
-
-         
-
-         // Output book information to the console
-
-         book.DisplayInfo();
-         
-
-
-
+        book.DisplayInfo();
     }
-
 }
